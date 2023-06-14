b2c551094be18927e6c028548a20a88f3c1f9600
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-14 16:17:49 +0900

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by 飯塚政美 on 2023/06/14.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
